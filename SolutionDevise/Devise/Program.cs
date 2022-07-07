744c98d02a2c766094bd72a76a0d1c874c511228
Matt <matt99j@gmail.com>
2022-07-07 20:08:11 +0200

    added more customised exceptions, finished math round up

--- SolutionDevise/Devise/Program.cs
+++ SolutionDevise/Devise/Program.cs
@@ -1,17 +1,30 @@
 ﻿using Devise;
+using FileNotFoundException = Devise.FileNotFoundException;
 
-var filepath = args.Length > 0 ? Environment.GetCommandLineArgs()[1] : "./devise.txt";
-var file = FileParser.ParseFile(filepath);
-
-var resolver = new ShortestPathResolver(file.From, file.To, file.Conversions);
-
-var shortestConversionPath = resolver.ComputeDijkstra();
-var result = file.Amount;
-
-foreach (var conversion in shortestConversionPath)
+try
 {
-    Console.WriteLine($"Convert from {conversion.From} to {conversion.To} : {conversion.Rate} * {result}"); 
-    result = (float) Math.Round((conversion.Rate * result), 4);
-}
+    var filepath = args.Length > 0 ? Environment.GetCommandLineArgs()[1] : "./devise.txt";
+    var file = FileParser.ParseFile(filepath);
 
-Console.WriteLine($"The result for {file.Amount} {file.From} into {file.To} is {result} {file.To}");
+
+    var resolver = new ShortestPathResolver(file.From, file.To, file.Conversions);
+
+    var shortestConversionPath = resolver.ComputeDijkstra();
+    var result = file.Amount;
+
+    foreach (var conversion in shortestConversionPath)
+    {
+        Console.WriteLine($"Convert from {conversion.From} to {conversion.To} : {conversion.Rate} * {result}");
+        result = (float)Math.Round((conversion.Rate * result), 4);
+    }
+
+    Console.WriteLine($"The result for {file.Amount} {file.From} into {file.To} is {Math.Ceiling(result)} {file.To}");
+}
+catch (FileEmptyException)
+{
+    Console.WriteLine("File seems to be empty !"); 
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("File was not there !");
+}
